45aa8c1ba5af20bfac9dbbbee63eb7dcacd3ecb9
MrPravinS <shegamwarpravin07@gmail.com>
2025-07-16 00:23:03 +0530

    build movies store using redux

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,21 +1,8 @@
-import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const nowPlatingMovies = async () => {
-    const res = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
-      API_OPTIONS
-    );
-    const data = await res.json()
-    console.log(data.results);
-    
-  };
-
-  useEffect(()=>{
-    nowPlatingMovies()
-  },[])
+   useNowPlayingMovies()
   return (
     <div>
       <Header />
---
+++ src/hooks/useNowPlayingMovies.js
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { useEffect } from "react";
+
+const useNowPlayingMovies = () => {
+    const dispatch = useDispatch()
+  const nowPlatingMovies = async () => {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+      API_OPTIONS
+    );
+    const data = await res.json()
+    console.log(data.results);
+    dispatch(addNowPlayingMovies(data.results))
+    
+  };
+
+  useEffect(()=>{
+    nowPlatingMovies()
+  },[])
+}
+
+
+export default useNowPlayingMovies
